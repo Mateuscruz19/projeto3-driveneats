90a02112c6e7d834264929ac00fd632cb25e963b
Mateuscruz19 <fdsmeuirmao2@outlook.com>
2022-08-15 15:22:29 -0300

    bugsp3

--- scripts.js
+++ scripts.js
@@ -6,12 +6,10 @@ let principall;
 
     const botaoexcluir = document.querySelector('.franguinho .selecionado')
     const iconeselecionado = document.querySelector('.selecionado .icone')
-    const produtinho = document.querySelector('.franguinho .titulo-frango')
 
     if (botaoexcluir !== null){ 
     botaoexcluir.classList.remove('selecionado')
     iconeselecionado.classList.add('escondido')
-    produtinho.innerHTML = ""
      }
      
     const seletor = '.'+botaoselecionado;
@@ -19,19 +17,15 @@ let principall;
     botao.classList.add('selecionado')
     const iconeescondido = document.querySelector('.selecionado .escondido')
 
-    iconeescondido.classList.remove ('escondido')
 
-    produto1 = produtinho.innerHTML;
+    iconeescondido.classList.remove ('escondido') 
     principall = botao.innerHTML;
-     
-
-    console.log(produto1)
     habilitaBotaoContinuar ()
  }
 
  let bebida;
 
- function selecionarbebida(botaoselecionadoo){
+ function selecionarbebida(botaoselecionado){
 
    const botaoexcluir = document.querySelector('.franguinho2 .selecionado')
    const iconeselecionado = document.querySelector('.selecionado .icone')
@@ -41,15 +35,14 @@ let principall;
    iconeselecionado.classList.add('escondido')
     }
     
-   const seletor = '.'+botaoselecionadoo;
+   const seletor = '.'+botaoselecionado;
    const botao = document.querySelector(seletor);
    botao.classList.add('selecionado')
    const iconeescondido = document.querySelector('.selecionado .escondido')
 
    iconeescondido.classList.remove ('escondido')
-
+    bebida = botao.innerHTML;
    habilitaBotaoContinuar ()
-   bebida = botao.innerHTML;
 }
 
  let sobremesa;
@@ -70,34 +63,32 @@ let principall;
     const iconeescondido = document.querySelector('.selecionado .escondido')
 
     iconeescondido.classList.remove ('escondido')
-    sobremesa = botao.innerHTML
 
+    sobremesa = botao.innerHTML
     habilitaBotaoContinuar ()
  }
 
 
 
  function habilitaBotaoContinuar (){
-   if(principall !== undefined){
-       if(bebida !== undefined){
-           if(sobremesa !== undefined){
+   if(principall !== undefined && bebida !== undefined && sobremesa !== undefined){
                const botaoContinuar = document.querySelector('.botaofinal');
+               botaoContinuar.classList.add('enable')
                botaoContinuar.classList.add('botaoverde');
-               botaoContinuar.innerHTML = 'Fechar Pedido'
+               botaoContinuar.innerHTML = 'Fechar Pedido';   
                
            }
-       }
    }
-   
 
 function funcaowhatsapp(){
    if(habilitaBotaoContinuar !== undefined ){  
        let encoded = encodeURIComponent(`Olá, gostaria de fazer o pedido:
-   - Prato: Frango Yin Yang 
-   - Bebida: Coquinha Gelada 
-   - Sobremesa: Pudim 
-   Total: R$ 27.70`)
+   - Prato: ${principall}
+   - Bebida: ${bebida}
+   - Sobremesa: ${sobremesa}
+   Total: R$ 59.94`)
        window.open('https://wa.me/5575998741647?text='+encoded)
-   }
+       
+    }
+
  }
-}
