9f82ee78a938b38de72915b17f48ededf858a963
Mateuscruz19 <fdsmeuirmao2@outlook.com>
2022-08-15 00:09:44 -0300

    check e borda pronta

--- scripts.js
+++ scripts.js
@@ -1,47 +1,59 @@
 
+
  function selecionarfrango(botaoselecionado){
 
-    const botaoexcluir = document.querySelector('.franguinho .selecionado ')
-
+    const botaoexcluir = document.querySelector('.franguinho .selecionado')
+    const iconeselecionado = document.querySelector('.selecionado .icone')
 
     if (botaoexcluir !== null){ 
     botaoexcluir.classList.remove('selecionado')
+    iconeselecionado.classList.add('escondido')
      }
-
+     
     const seletor = '.'+botaoselecionado;
     const botao = document.querySelector(seletor);
     botao.classList.add('selecionado')
+    const iconeescondido = document.querySelector('.selecionado .escondido')
+
+    iconeescondido.classList.remove ('escondido')
  }
+
+
 
 
  function selecionarbebida(botaoselecionado){
 
     const botaoexcluir = document.querySelector('.franguinho2 .selecionado')
-
+    const iconeselecionado = document.querySelector('.selecionado .icone')
 
     if (botaoexcluir !== null){ 
     botaoexcluir.classList.remove('selecionado')
+    iconeselecionado.classList.add('escondido')
      }
-
+     
     const seletor = '.'+botaoselecionado;
     const botao = document.querySelector(seletor);
     botao.classList.add('selecionado')
+    const iconeescondido = document.querySelector('.selecionado .escondido')
+
+    iconeescondido.classList.remove ('escondido')
  }
 
  function selecionarsobremesa(botaoselecionado){
 
     const botaoexcluir = document.querySelector('.franguinho3 .selecionado')
-
+    const iconeselecionado = document.querySelector('.selecionado .icone')
 
     if (botaoexcluir !== null){ 
     botaoexcluir.classList.remove('selecionado')
+    iconeselecionado.classList.add('escondido')
      }
-
+     
     const seletor = '.'+botaoselecionado;
     const botao = document.querySelector(seletor);
     botao.classList.add('selecionado')
+    const iconeescondido = document.querySelector('.selecionado .escondido')
+
+    iconeescondido.classList.remove ('escondido')
  }
-
-
-
  
